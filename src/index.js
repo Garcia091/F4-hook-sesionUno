aee01bc4d8e51a244adb586567706bcea4d7eb95
Silvia Garcia <garcia.s091@gmail.com>
2021-09-07 22:01:14 -0500

    Hook

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './1_useState/components/Counter'
+//import CounterCustom from './1_useState/components/CounterCustom'
+//import Formulario from './2_useEffect/components/Formulario'
+//import ApiFect from './2_useEffect/components/ApiFect'
+//import RickMortyApp from './3_practica/RickMortyApp'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+ReactDOM.render(
+    <Counter />,
+  document.getElementById('root')
+);
+
